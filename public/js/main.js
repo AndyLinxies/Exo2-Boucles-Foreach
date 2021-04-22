2ec31e39667eff95a11a234fb6b4c1c3d6f72f32
AndyLinxies <linxies@gmail.com>
2021-04-22 09:07:45 +0200

    Exo-2-Boucles-Foreach

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+// //Exo1
+// let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+// eleves.forEach(element => {
+//     console.log("Bonjour "+element)
+// });
+
+//Exo2
+// let monArray = ["orange","banane","mangue","citron","pamplemousse"];
+
+// monArray.forEach(element => {
+//     console.log(element);
+// });
+
+//Exo perso
+// let monArray = ["orange","banane","mangue","citron","pamplemousse"];
+
+// monArray.forEach(element => {
+//     console.log(element.toLocaleUpperCase());
+// });
